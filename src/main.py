9005d7198be53aa1bd0f67552902b6a762b19bce
Luis Ortiz <luisjoseortiz.ortiz@gmail.com>
2026-08-12 11:44:36 -0600

    add event routes to the main fastapi app

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 
+from src.routes.events import router as events_router
 from src.routes.users import router as user_router
 
 app = FastAPI()
 
 # Register routes here
-app.include_router(user_router, prefix="/user")
+app.include_router(user_router)
+app.include_router(events_router)
