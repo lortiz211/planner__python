c6d6206fa7a9ff6f5a0ed23e838f71e61d055472
Luis Ortiz <luisjoseortiz.ortiz@gmail.com>
2026-08-13 16:02:06 -0600

    add database connection

---
+++ src/database/connection.py
@@ -0,0 +1,21 @@
+from sqlmodel import Session, SQLModel, create_engine
+
+from src.models.events import Event  # noqa: F401
+
+database_file = "planner.db"
+database_connection_string = f"sqlite:///{database_file}"
+connect_args = {"check_same_thread": False}
+engine_url = create_engine(
+    url=database_connection_string,
+    echo=True,
+    connect_args=connect_args,
+)
+
+
+def conn():
+    SQLModel.metadata.create_all(engine_url)
+
+
+def get_session():
+    with Session(engine_url) as session:
+        yield session
