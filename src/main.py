ded28c536de1bdda95421029d2b6dd26918c912e
Luis Ortiz <luisjoseortiz.ortiz@gmail.com>
2026-08-13 16:06:24 -0600

    add db connection to the start of the project

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
+from src.database.connection import conn
 from src.routes.events import router as events_router
 from src.routes.users import router as user_router
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    conn()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 # Register routes here
 app.include_router(user_router)
